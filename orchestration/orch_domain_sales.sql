cbaae77012a3f29223de9bcf0efd54480716e2a8
jdcorchuelom <jdcorchuelom@gmail.com>
2025-05-03 12:03:06 -0500

    change name in sql files

--- orchestration/orch_domain_sales.sql
+++ orchestration/orch_domain_sales.sql
@@ -1,31 +1,31 @@
 
-CALL `premium-guide-410714.silver_transaction.sp_load_product`
+CALL `dev-arquitectura-jcorchuelom.silver_transaction.sp_load_product`
 (
-  'premium-guide-410714.silver_transaction.product',
+  'dev-arquitectura-jcorchuelom.silver_transaction.product',
   'bigquery-public-data.thelook_ecommerce.products' ,
-  'premium-guide-410714.operation.tables'
+  'dev-arquitectura-jcorchuelom.operation.tables'
 );
 
-CALL `premium-guide-410714.silver_transaction.sp_load_transaction`
+CALL `dev-arquitectura-jcorchuelom.silver_transaction.sp_load_transaction`
 (
   '2025-04-01',
   '2025-04-30',
-  'premium-guide-410714.silver_transaction.transaction',
+  'dev-arquitectura-jcorchuelom.silver_transaction.transaction',
   'bigquery-public-data.thelook_ecommerce.order_items'
 ) ;
 
 
-CALL `premium-guide-410714.gold_ba.sp_load_ba_sales`
+CALL `dev-arquitectura-jcorchuelom.gold_ba.sp_load_ba_sales`
 (
   '2025-04' ,
-  'premium-guide-410714.gold_ba.ba_sales',
-  'premium-guide-410714.silver_transaction.transaction',
-  'premium-guide-410714.silver_transaction.product'
+  'dev-arquitectura-jcorchuelom.gold_ba.ba_sales',
+  'dev-arquitectura-jcorchuelom.silver_transaction.transaction',
+  'dev-arquitectura-jcorchuelom.silver_transaction.product'
 ) ;
 
-CALL `premium-guide-410714.quality.sp_load_quantitative`
+CALL `dev-arquitectura-jcorchuelom.quality.sp_load_quantitative`
 (
-   'premium-guide-410714.quality.quantitative',
-  'premium-guide-410714.gold_ba.ba_sales',
+   'dev-arquitectura-jcorchuelom.quality.quantitative',
+  'dev-arquitectura-jcorchuelom.gold_ba.ba_sales',
   '2025-04-01'
 ) ;
